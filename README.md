9e0de9b7a9864c81fece9a1082e3fbd8ff1dffab
conglytran0301 <congly.work@gmail.com>
2024-10-03 08:46:49 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# TailwindCSS Coffee Style by TrungQuanDev
+
+## Requirements
+
+* **nodejs >= v14.7.0**
+* **npm >= v6.14.7**
+* **yarn >= v1.19.1**
+
+Clone project and run test.
+
+```
+$ git clone https://git_url_clone <project_dir>
+$ cd <project_dir>
+$ yarn build:css
+$ yarn start:dev
+```
+
+### Guide for this repository is here:
+
+Guide: https://youtube.com/playlist?list=PLP6tw4Zpj-RK9PxvjySFM7jc5aNn_RyLX
+
+Demo: https://coffee-style-trungquandev.web.app/
+
+### About Me - Thông tin về mình:
+
+Author: **Trungquandev - Một Lập Trình Viên** & **CodeTQ**
+
+Blog: https://trungquandev.com/
+
+CV: https://cv.trungquandev.com/
+
+YouTube 01 (Trungquandev - Một Lập Trình Viên): https://www.youtube.com/@trungquandev
+
+YouTube 02 (CodeTQ): https://www.youtube.com/@code-tq
+
+"Learning new everyday not the copycat of yesterday!"
+
+"A bit of fragrance clings to the hand that gives flowers!"
+
+Thanks for watching!
---
+++ public/main.js
@@ -0,0 +1,29 @@
+/**
+ * Author: TrungQuanDev
+ * YouTube: https://www.youtube.com/@trungquandev
+ * Blog: https://trungquandev.com
+ * CV: https://cv.trungquandev.com
+ */
+
+// document.onreadystatechange = () => {
+//   if (document.readyState == 'complete') {
+//     alert('Quick note: This website is a learning project in the field of programming. All information in this website are sample data, they are not real. Thanks for reading this note!')
+//   }
+// }
+
+const topMenu = document.getElementById('tqd-top-menu')
+const toggleTopMenuIcon = document.getElementById('tqd-toggle-top-menu-icon')
+
+document.addEventListener('click', (e) => {
+  if (toggleTopMenuIcon.contains(e.target)) {
+    // Click to Toggle top menu icon
+    topMenu.classList.toggle('tqd-topmenu-expanded')
+    topMenu.classList.toggle('hidden')
+  } else {
+    // Click Outside from Toggle top menu icon
+    if (topMenu.classList.contains('tqd-topmenu-expanded')) {
+      topMenu.classList.remove('tqd-topmenu-expanded')
+      topMenu.classList.add('hidden')
+    }
+  }
+})
